ab57bc141336db9b9476ceb9e4edc600c12e588d
RobQocharyan <robqocharyan@mail.ru>
2023-08-30 23:04:30 +0400

    second commit

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -27,16 +27,37 @@ export const Work = () => {
     {
       id:2,
       title: "Card 2",
-      content: "This is the content of card 1.",
+      content: "This is the content of card 2.",
       hrefImg:require("../../assets/online-shop.png"),
       hrefLink:"https://tovaradar.ru/"
     },
     {
       id:3,
       title: "Card 3",
-      content: "This is the content of card 1.",
+      content: "This is the content of card 3.",
       hrefImg:require("../../assets/todo.png"),
       hrefLink:"https://robqocharyan123.github.io/todo/"
+    },
+    {
+      id:4,
+      title: "Card 4",
+      content: "This is the content of card 4.",
+      hrefImg:require("../../assets/pinnacle.png"),
+      hrefLink:"https://robqocharyan123.github.io/lunafox/"
+    },
+    {
+      id:5,
+      title: "Card 5",
+      content: "This is the content of card 5.",
+      hrefImg:require("../../assets/mogo.png"),
+      hrefLink:"https://robqocharyan123.github.io/"
+    },
+    {
+      id:5,
+      title: "Card 6",
+      content: "This is the content of card 6.",
+      hrefImg:require("../../assets/food.jpg"),
+      hrefLink:"https://robqocharyan.github.io/"
     }
   ]);
   return (
